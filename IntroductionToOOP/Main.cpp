28e96315e26eb41866653682ec16b60d7d42fee3
Nickshe11 <shelobod@gmail.com>
2022-10-22 21:17:09 +0300

    Operators overload

--- IntroductionToOOP/Main.cpp
+++ IntroductionToOOP/Main.cpp
@@ -62,9 +62,30 @@ public:
 		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
 	}
+	Point& operator++()//Prefix increment
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int/*здесь возможен только int*/) //Postfix increment (Suffix increment)
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
+
+	Point& operator()(double x, double y)
+	{
+		set_x(x);
+		set_y(y);
+		return *this;
+	}
 
 	// Methods:
-	 double distance(const Point& other) const
+
+	double distance(const Point& other) const
 	{
 		/*double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
@@ -80,10 +101,51 @@ public:
 
 double distance(const Point& A, const Point& B);
 
+Point operator+ (const Point& left, const Point& right)
+{
+	Point res;
+	res.set_x(left.get_x() + right.get_x());
+	res.set_y(left.get_y() + right.get_y());
+	return res;
+}
+
+bool operator==(const Point& left, const Point& right)
+{
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		return true;
+	else
+		return false;*/
+}
+
+bool operator !=(const Point& left, const Point& right)
+{
+	return !(left == right);
+}
+
+bool operator<(const Point& left, const Point& right)
+{
+	return left.get_x() < right.get_x() && left.get_y() < right.get_y();
+}
+
+bool operator<=(const Point& left, const Point& right)
+{
+	return left.get_x() <= right.get_x() && left.get_y() <= right.get_y();
+}
+
+ostream& operator<<(ostream& os, const Point& obj)
+{
+	os << "X = " << obj.get_x();
+	os << "\tY = " << obj.get_y();
+	return os;
+}
 //#define STRUCT_POINT
 //#define GET_SET_CHECK
 //#define CONSTRUCTORS_CHECK
 //#define DISTANCE_CHECK
+//#define ASSIGNMENT_OPERATOR_CHECK
+//#define ARITHMETICAL_OPERATORS_CHECK
+//#define COMPARISON_OPERATORS
 
 void main()
 {
@@ -156,6 +218,7 @@ void main()
 	E.print();
 #endif // CONSTRUCTORS_CHECK
 
+#ifdef ASSIGNMENT_OPERATOR_CHECK
 	int a, b, c;
 	a = b = c = 0;
 
@@ -163,6 +226,35 @@ void main()
 	cout << delimiter << endl;
 	A = B = C = Point(2, 3);
 	cout << delimiter << endl;
+#endif // ASSIGNMENT_OPERATOR_CHECK
+
+#ifdef ARITHMETICAL_OPERATORS_CHECK
+	Point A(2, 3);
+	Point B(7, 8);
+	Point C = A + B;
+	A.print();
+	B.print();
+	C.print();
+	Point D = C++;
+	C.print();
+	D.print();
+#endif // ARITHMETICAL_OPERATORS_CHECK
+#ifdef COMPARISON_OPERATORS
+	cout << (Point(2, 3) == Point(2, 3)) << endl;
+
+	for (Point i = 0; i < Point(10, 10); ++i)
+	{
+		i.print();
+	}
+#endif // COMPARISON_OPERATORS
+
+	Point A;
+	//A.print();
+	/*A.set_x(2);
+	A.set_y(3);*/
+	A(2, 3);
+	//A.print();
+	cout << A << endl;
 }
 
 double distance(const Point& A, const Point& B)
