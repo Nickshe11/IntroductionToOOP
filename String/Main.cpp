029ea1ddef2f1607d6b4fd0727a399b9eb8a899a
Nickshe11 <shelobod@gmail.com>
2022-11-04 20:50:17 +0300

    Delegation DONE

--- String/Main.cpp
+++ String/Main.cpp
@@ -48,19 +48,19 @@ public:
 		this->str = new char[size] {};
 		cout << "Constructor:\t" << this << endl;
 	}*/
-	String(const char str[]):size(strlen(str)+1), str (new char [size] {})
+	String(const char str[]):String(strlen(str)+1)
 	{
 		//this->size = strlen(str) + 1; // +1 на терминирующий ноль
 		//this->str = new char [size] {};
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t" << endl;
 	}
-	String(const String& other) : size(other.size), str(new char[size]{})
+	String(const String& other) : String(other.str)
 	{
 		//Deep copy - побитовое копирование
 		/*this->size = other.size;
 		this->str = new char[size] {};*/
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		//for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstructor:" << this << endl;
 	}
 	String(String&& other):size(other.size), str(other.str)
